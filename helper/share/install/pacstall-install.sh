53853e614405ece33e9d2a2ea133b6a7c5247e87
samwhelp <samwhelp@users.noreply.github.com>
2023-08-12 11:15:05 +0800

    fix

---
+++ helper/share/install/pacstall-install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+##
+## https://pacstall.dev/
+## https://github.com/pacstall/pacstall/wiki/Pacscript-101
+##
+
+sudo bash -c "$(curl -fsSL https://pacstall.dev/q/install)"
+
